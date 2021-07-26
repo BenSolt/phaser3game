fb0013b08a70819d2c9e27d919a0480b6510db2d
benjamin-solt <bensolt@gmail.com>
2021-07-26 09:55:46 -0400

    created player

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -27,6 +27,9 @@ export default class GameScene extends Phaser.Scene
 	{
 		this.add.image(400, 300, 'sky')
     	this.add.image(400, 300, 'star')
+
+        this.createPlatforms()
+        this.createPlayer()
 	}
 
     createPlatforms()
@@ -38,4 +41,31 @@ export default class GameScene extends Phaser.Scene
 		platforms.create(50, 250, GROUND_KEY)
 		platforms.create(750, 220, GROUND_KEY)
     }
+
+    createPlayer()
+	{
+		this.player = this.physics.add.sprite(100, 450, DUDE_KEY)
+		this.player.setBounce(0.2)
+		this.player.setCollideWorldBounds(true)
+
+		this.anims.create({
+			key: 'left',
+			frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 0, end: 3 }),
+			frameRate: 10,
+			repeat: -1
+		})
+		
+		this.anims.create({
+			key: 'turn',
+			frames: [ { key: DUDE_KEY, frame: 4 } ],
+			frameRate: 20
+		})
+		
+		this.anims.create({
+			key: 'right',
+			frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 5, end: 8 }),
+			frameRate: 10,
+			repeat: -1
+		})
+	}
 }
