d6de5f3ccaae3c01279497df7e610e6f8bee6ee1
benjamin-solt <bensolt@gmail.com>
2021-07-26 09:51:04 -0400

    adding const keys

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,8 @@
 import Phaser from 'phaser'
 
+const GROUND_KEY = 'ground'
+const DUDE_KEY = 'dude'
+
 export default class GameScene extends Phaser.Scene
 {
 	constructor()
@@ -10,7 +13,7 @@ export default class GameScene extends Phaser.Scene
 	preload()
 	{
         this.load.image('sky', 'assets/sky.png')
-		this.load.image('ground', 'assets/platform.png')
+		this.load.image(GROUND_KEY, 'assets/platform.png')
 		this.load.image('star', 'assets/star.png')
 		this.load.image('bomb', 'assets/bomb.png')
 
@@ -25,4 +28,14 @@ export default class GameScene extends Phaser.Scene
 		this.add.image(400, 300, 'sky')
     	this.add.image(400, 300, 'star')
 	}
+
+    createPlatforms()
+    {
+        const platforms = this.physics.add.staticGroup() 
+        platforms.create(400, 568, GROUND_KEY).setScale(2).refreshBody()
+	
+		platforms.create(600, 400, GROUND_KEY)
+		platforms.create(50, 250, GROUND_KEY)
+		platforms.create(750, 220, GROUND_KEY)
+    }
 }
